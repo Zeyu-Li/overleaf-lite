40b8512e1127ee6e672e63e686d757e6684b12eb
Zeyu-Li <zeyu.li23@gmail.com>
2021-10-17 11:55:17 -0600

    integrated monaco editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { CodeModel } from '@ngstack/code-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'overleaf-lite';
+  theme = 'vs-dark';
+
+  codeModel: CodeModel = {
+    language: 'javascript',
+    uri: 'main.json',
+    value: 'const a = () => {c.l()}',
+  };
+
+  options = {
+    contextmenu: true,
+    minimap: {
+      enabled: true,
+    },
+  };
+
+  onCodeChanged(value) {
+    console.log('CODE', value);
+  }
 }
