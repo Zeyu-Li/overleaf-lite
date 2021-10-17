4cffdd847e2fd3d37ebaa433f9623466a7fd7bb2
Zeyu-Li <zeyu.li23@gmail.com>
2021-10-17 12:07:54 -0600

    incorporated katex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { CodeModel } from '@ngstack/code-editor';
 
+const startingText = `
+% this is a realtime LaTeX editor
+`;
+
+let beginningText = localStorage.getItem('beginningText')
+  ? localStorage.getItem('beginningText')
+  : startingText;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'overleaf-lite';
+  // title = 'overleaf-lite';
+  equation: string = '\\sum_{i=1}^nx_i';
   theme = 'vs-dark';
 
   codeModel: CodeModel = {
-    language: 'javascript',
+    language: 'latex',
     uri: 'main.json',
-    value: 'const a = () => {c.l()}',
+    value: beginningText,
   };
 
   options = {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { KatexModule } from 'ng-katex';
 import { AppComponent } from './app.component';
 import { CodeEditorModule } from '@ngstack/code-editor';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CodeEditorModule.forRoot()],
+  imports: [BrowserModule, KatexModule, CodeEditorModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
