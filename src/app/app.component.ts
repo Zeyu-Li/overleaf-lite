d454eb7a0d64e27ddc746b698fffc396ca0e8194
Zeyu-Li <zeyu.li23@gmail.com>
2021-10-17 13:16:31 -0600

    download button complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CodeModel } from '@ngstack/code-editor';
+import { saveAs } from 'file-saver';
 
 const startingText = `
-% this is a realtime LaTeX editor!
-% made with AngularJS
+% Overleaf Lite a realtime LaTeX editor!
+% made with AngularJS (Autosaves every 30 seconds)
 % see the source code here: https://github.com/Zeyu-Li/overleaf-lite
 
 \\pi = 3
@@ -37,6 +38,11 @@ export class AppComponent {
   };
 
   onCodeChanged(value) {
-    this.equation = value;
+    // console.log(value, value.split('\n').join('\n\\newline '));
+    this.equation = value.split('\n').join('\n\\\\ ');
+  }
+  downloadLatex() {
+    var blob = new Blob([this.equation], { type: 'text/plain;charset=utf-8' });
+    saveAs(blob, 'file.tex');
   }
 }
